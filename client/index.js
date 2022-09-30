2b9d2f26f6e69a51cb8fe7ecadc12ce161fa5f46
Nico Rehwaldt <git_nikku@nixis.de>
2022-09-30 14:35:03 +0200

    chore: document lint pattern / register for C7, too

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ import { registerClientPlugin } from 'camunda-modeler-plugin-helpers';
 
 import { config, resolver } from '../.bpmnlintrc';
 
+// provide { config, resolver } as a `lintRules.${tabType}` plug-in
 registerClientPlugin({ config, resolver }, 'lintRules.cloud-bpmn');
+registerClientPlugin({ config, resolver }, 'lintRules.bpmn');
